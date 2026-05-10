2d6a8374b4e311daca99c45aac26338ca92fba5b
bradjacobs <bradjacobs@gmail.com>
2026-05-10 11:29:58 -0700

    add top level comment on new classes.

--- src/test/java/com/github/bradjacobs/excel/api/SheetContentValidationTest.java
+++ src/test/java/com/github/bradjacobs/excel/api/SheetContentValidationTest.java
@@ -1,3 +1,6 @@
+/*
+ * This file is subject to the terms and conditions defined in the 'LICENSE' file.
+ */
 package com.github.bradjacobs.excel.api;
 
 import org.junit.jupiter.params.ParameterizedTest;
