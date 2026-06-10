305b5438d4a006b6499dfc18e8e7375936a1fb10
bradjacobs <bradjacobs@gmail.com>
2026-06-09 18:42:33 -0700

    add unittest for EventModelExcelReader for exception for unsopported old file type

--- src/test/java/com/github/bradjacobs/excel/engine/eventmodel/EventModelExcelReaderTest.java
+++ src/test/java/com/github/bradjacobs/excel/engine/eventmodel/EventModelExcelReaderTest.java
@@ -4,12 +4,37 @@
 package com.github.bradjacobs.excel.engine.eventmodel;
 
 import com.github.bradjacobs.excel.engine.AbstractExcelReaderTest;
+import com.github.bradjacobs.excel.request.ExcelReadRequest;
+import com.github.bradjacobs.excel.testutils.TestResourceUtil;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class EventModelExcelReaderTest extends AbstractExcelReaderTest<EventModelExcelReader, EventModelExcelReader.Builder> {
 
     @Override
     protected EventModelExcelReader.Builder createBuilder() {
         return EventModelExcelReader.builder();
+    }
+
+    @Test
+    public void oldFileFormatThrowsException() {
+        // event reader doesn't support older formats,
+        // thus expect exception thrown if given old file format.
+        Path oldFilePath = TestResourceUtil.getResourceFilePath("test_data.xls");
+
+        Exception thrown = assertThrows(IOException.class, () -> {
+            EventModelExcelReader reader = createBuilder().build();
+            ExcelReadRequest request = ExcelReadRequest.from(oldFilePath).build();
+            reader.readSheet(request);
+        });
+        assertEquals("Failed to read Excel sheet data: " +
+                        "Cannot open workbook - unsupported file type: OLE2",
+                thrown.getMessage());
     }
 
     // TODO
